e935ca20b05b8402d69e642704ab0ce7b38e93d7
caubekimo@gmail.com <caubekimo@gmail.com>
2021-10-18 23:23:03 +0800

    把多餘的註釋去掉

--- hw1/PythonNltk/PythonNltk/PythonNltk.py
+++ hw1/PythonNltk/PythonNltk/PythonNltk.py
@@ -8,32 +8,20 @@ from flask import jsonify, request
 
 app = flask.Flask(__name__)
 
-#@app.route('/Nltk', methods=['GET'])
-#def Nltk():
-#    if 'content' in request.args:
-#        content = request.args['content']
-
-#        blog = {'char': 'datacamp.com', 'name': 'Datacamp'}
-
-#        return 'There are ' + str(len(sent_tokenize(content))) + ' sentences
-#        in your content.'
-#    else:
-#        return 'Error: No content provided.  Please specify a content.'
-
 @app.route('/TextStat', methods=['GET'])
 def TextStat():
     if 'content' in request.args:
         content = request.args['content']
-        charCount = textstat.char_count(content, ignore_spaces=True)
-        wordCount = len(jieba.lcut(content))
-        sentenceCount = len(sent_tokenize(content))
+
+        charCount = textstat.char_count(content, ignore_spaces=True) #利用 textstat 計算字元數
+        wordCount = len(jieba.lcut(content)) #利用 jieba 計算字數
+        sentenceCount = len(sent_tokenize(content)) #利用 Nltk 計算句數
 
         stat = [{'char': charCount,'word': wordCount,'sentence': sentenceCount}]
-        return json.dumps(stat)
+        return json.dumps(stat) #回傳 JSON 格式的結果
 
     else:
         return 'Error: No content provided. Please specify a content.'
-
 
 
 app.run()
